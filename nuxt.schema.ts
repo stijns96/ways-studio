03aa94adbb0bfde33c7e86166b46e4bbdc171501
Stijn <stijn@code.nl>
2024-12-09 22:06:36 +0100

    Enhance app configuration by adding a parent group with fields in nuxt.schema.ts

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,5 +1,20 @@
 import { field, group } from "@nuxt/content/studio";
 
 export default defineNuxtSchema({
-  appConfig: {},
+  appConfig: {
+    parent: group({
+      title: "Parent title",
+      description: "Parent description.",
+      icon: "i-icon-to-display",
+      fields: {
+        leaf: field({
+          type: "string",
+          title: "Field title",
+          description: "Field Description",
+          icon: "i-icon-to-display",
+          default: "default value",
+        }),
+      },
+    }),
+  },
 });
